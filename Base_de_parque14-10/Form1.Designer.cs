ec268a514724312df449d74ca72c178cab90118e
Cuca <Cuca@DESKTOP-ABVL2QB>
2024-10-14 20:05:56 -0300

    Agregar archivos de proyecto.

---
+++ Base_de_parque14-10/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace Restaurant
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblIncidente = new System.Windows.Forms.Label();
+            this.txtIncidente = new System.Windows.Forms.TextBox();
+            this.txtEmpleado = new System.Windows.Forms.TextBox();
+            this.lblEmpleado = new System.Windows.Forms.Label();
+            this.txtZona = new System.Windows.Forms.TextBox();
+            this.lblZona = new System.Windows.Forms.Label();
+            this.txtTipo = new System.Windows.Forms.TextBox();
+            this.lblTipo = new System.Windows.Forms.Label();
+            this.txtFin = new System.Windows.Forms.TextBox();
+            this.lblFin = new System.Windows.Forms.Label();
+            this.txtInicio = new System.Windows.Forms.TextBox();
+            this.lblInicio = new System.Windows.Forms.Label();
+            this.txtFecha = new System.Windows.Forms.TextBox();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Incidente = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Empleado = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Zona = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tipo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Fecha = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Inicio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Fin = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblIncidente
+            // 
+            this.lblIncidente.AutoSize = true;
+            this.lblIncidente.Location = new System.Drawing.Point(29, 51);
+            this.lblIncidente.Name = "lblIncidente";
+            this.lblIncidente.Size = new System.Drawing.Size(66, 13);
+            this.lblIncidente.TabIndex = 0;
+            this.lblIncidente.Text = "Incidente N°";
+            // 
+            // txtIncidente
+            // 
+            this.txtIncidente.Location = new System.Drawing.Point(110, 48);
+            this.txtIncidente.Name = "txtIncidente";
+            this.txtIncidente.Size = new System.Drawing.Size(100, 20);
+            this.txtIncidente.TabIndex = 1;
+            // 
+            // txtEmpleado
+            // 
+            this.txtEmpleado.Location = new System.Drawing.Point(110, 82);
+            this.txtEmpleado.Name = "txtEmpleado";
+            this.txtEmpleado.Size = new System.Drawing.Size(100, 20);
+            this.txtEmpleado.TabIndex = 3;
+            // 
+            // lblEmpleado
+            // 
+            this.lblEmpleado.AutoSize = true;
+            this.lblEmpleado.Location = new System.Drawing.Point(29, 85);
+            this.lblEmpleado.Name = "lblEmpleado";
+            this.lblEmpleado.Size = new System.Drawing.Size(54, 13);
+            this.lblEmpleado.TabIndex = 2;
+            this.lblEmpleado.Text = "Empleado";
+            // 
+            // txtZona
+            // 
+            this.txtZona.Location = new System.Drawing.Point(110, 117);
+            this.txtZona.Name = "txtZona";
+            this.txtZona.Size = new System.Drawing.Size(100, 20);
+            this.txtZona.TabIndex = 5;
+            // 
+            // lblZona
+            // 
+            this.lblZona.AutoSize = true;
+            this.lblZona.Location = new System.Drawing.Point(29, 120);
+            this.lblZona.Name = "lblZona";
+            this.lblZona.Size = new System.Drawing.Size(32, 13);
+            this.lblZona.TabIndex = 4;
+            this.lblZona.Text = "Zona";
+            // 
+            // txtTipo
+            // 
+            this.txtTipo.Location = new System.Drawing.Point(110, 152);
+            this.txtTipo.Name = "txtTipo";
+            this.txtTipo.Size = new System.Drawing.Size(100, 20);
+            this.txtTipo.TabIndex = 7;
+            // 
+            // lblTipo
+            // 
+            this.lblTipo.AutoSize = true;
+            this.lblTipo.Location = new System.Drawing.Point(29, 155);
+            this.lblTipo.Name = "lblTipo";
+            this.lblTipo.Size = new System.Drawing.Size(28, 13);
+            this.lblTipo.TabIndex = 6;
+            this.lblTipo.Text = "Tipo";
+            // 
+            // txtFin
+            // 
+            this.txtFin.Location = new System.Drawing.Point(109, 259);
+            this.txtFin.Name = "txtFin";
+            this.txtFin.Size = new System.Drawing.Size(100, 20);
+            this.txtFin.TabIndex = 13;
+            // 
+            // lblFin
+            // 
+            this.lblFin.AutoSize = true;
+            this.lblFin.Location = new System.Drawing.Point(28, 262);
+            this.lblFin.Name = "lblFin";
+            this.lblFin.Size = new System.Drawing.Size(47, 13);
+            this.lblFin.TabIndex = 12;
+            this.lblFin.Text = "Hora Fin";
+            // 
+            // txtInicio
+            // 
+            this.txtInicio.Location = new System.Drawing.Point(109, 224);
+            this.txtInicio.Name = "txtInicio";
+            this.txtInicio.Size = new System.Drawing.Size(100, 20);
+            this.txtInicio.TabIndex = 11;
+            // 
+            // lblInicio
+            // 
+            this.lblInicio.AutoSize = true;
+            this.lblInicio.Location = new System.Drawing.Point(28, 227);
+            this.lblInicio.Name = "lblInicio";
+            this.lblInicio.Size = new System.Drawing.Size(58, 13);
+            this.lblInicio.TabIndex = 10;
+            this.lblInicio.Text = "Hora Inicio";
+            // 
+            // txtFecha
+            // 
+            this.txtFecha.Location = new System.Drawing.Point(109, 190);
+            this.txtFecha.Name = "txtFecha";
+            this.txtFecha.Size = new System.Drawing.Size(100, 20);
+            this.txtFecha.TabIndex = 9;
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Location = new System.Drawing.Point(28, 193);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(37, 13);
+            this.lblFecha.TabIndex = 8;
+            this.lblFecha.Text = "Fecha";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(77, 297);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 14;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Incidente,
+            this.Empleado,
+            this.Zona,
+            this.Tipo,
+            this.Fecha,
+            this.Inicio,
+            this.Fin});
+            this.dataGridView1.Location = new System.Drawing.Point(244, 106);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(742, 202);
+            this.dataGridView1.TabIndex = 15;
+            // 
+            // Incidente
+            // 
+            this.Incidente.HeaderText = "Incidente";
+            this.Incidente.Name = "Incidente";
+            // 
+            // Empleado
+            // 
+            this.Empleado.HeaderText = "Empleado";
+            this.Empleado.Name = "Empleado";
+            // 
+            // Zona
+            // 
+            this.Zona.HeaderText = "Zona";
+            this.Zona.Name = "Zona";
+            // 
+            // Tipo
+            // 
+            this.Tipo.HeaderText = "Tipo";
+            this.Tipo.Name = "Tipo";
+            // 
+            // Fecha
+            // 
+            this.Fecha.HeaderText = "Fecha";
+            this.Fecha.Name = "Fecha";
+            // 
+            // Inicio
+            // 
+            this.Inicio.HeaderText = "Inicio";
+            this.Inicio.Name = "Inicio";
+            // 
+            // Fin
+            // 
+            this.Fin.HeaderText = "Fin";
+            this.Fin.Name = "Fin";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.label1.Location = new System.Drawing.Point(459, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(527, 25);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "MÓDULO MONITOREO - PARQUE DE DIVERSIONES";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1009, 337);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txtFin);
+            this.Controls.Add(this.lblFin);
+            this.Controls.Add(this.txtInicio);
+            this.Controls.Add(this.lblInicio);
+            this.Controls.Add(this.txtFecha);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.txtTipo);
+            this.Controls.Add(this.lblTipo);
+            this.Controls.Add(this.txtZona);
+            this.Controls.Add(this.lblZona);
+            this.Controls.Add(this.txtEmpleado);
+            this.Controls.Add(this.lblEmpleado);
+            this.Controls.Add(this.txtIncidente);
+            this.Controls.Add(this.lblIncidente);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblIncidente;
+        private System.Windows.Forms.TextBox txtIncidente;
+        private System.Windows.Forms.TextBox txtEmpleado;
+        private System.Windows.Forms.Label lblEmpleado;
+        private System.Windows.Forms.TextBox txtZona;
+        private System.Windows.Forms.Label lblZona;
+        private System.Windows.Forms.TextBox txtTipo;
+        private System.Windows.Forms.Label lblTipo;
+        private System.Windows.Forms.TextBox txtFin;
+        private System.Windows.Forms.Label lblFin;
+        private System.Windows.Forms.TextBox txtInicio;
+        private System.Windows.Forms.Label lblInicio;
+        private System.Windows.Forms.TextBox txtFecha;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Incidente;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Empleado;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Zona;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tipo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Fecha;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Inicio;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Fin;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Base_de_parque14-10/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Base_de_parque14_10;
+using MySql.Data.MySqlClient;
+
+namespace Restaurant
+{
+
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            MostrarDatosEnGrilla();
+        }
+        public class Persona
+        {
+            public string Nombre;
+            public string Apellido;
+            public string ID;
+            public string Sector;
+            public string Contacto;
+
+            // Constructor de la clase
+            public Persona(string nombre, string apellido, string id, string sector, string contacto)
+            {
+                Nombre = nombre;
+                Apellido = apellido;
+                ID = id;
+                Sector = sector;
+                Contacto = contacto;
+            }
+            public Persona()
+            {
+                Nombre = "";
+                Apellido = "";
+                ID = "";
+                Sector = "";
+                Contacto = "";
+            }
+        }
+
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Obtén los valores de los cuadros de texto
+                string nombre = txtNombre.Text;
+                string apellido = txtApellido.Text;
+                string id = txtID.Text;
+                string sector = txtSector.Text;
+                string contacto = txtContacto.Text;
+
+                // Crea una instancia de la clase Persona con los valores
+                Persona nuevaPersona = new Persona(nombre, apellido, id, sector, contacto);
+                // Agrega la nueva Persona al DataGridView dgvPersonal
+
+                // Limpia los cuadros de texto después de agregar la Persona
+                txtNombre.Clear();
+                txtApellido.Clear();
+                txtID.Clear();
+                txtSector.Clear();
+                txtContacto.Clear();
+
+                // Cadena de conexión a la base de datos MySQL
+                string connectionString = "Server=localhost;Database=isft225;Uid=root;Pwd=";
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = "INSERT INTO isft225 (Nombre, Apellido, ID, Sector, Contacto) VALUES (@Nombre, @Apellido, @ID, @Sector, @Contacto)";
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        // Agregar parámetros
+                        command.Parameters.AddWithValue("@Nombre", nuevaPersona.Nombre);
+                        command.Parameters.AddWithValue("@Apellido", nuevaPersona.Apellido);
+                        command.Parameters.AddWithValue("@ID", nuevaPersona.ID);
+                        command.Parameters.AddWithValue("@Sector", nuevaPersona.Sector);
+                        command.Parameters.AddWithValue("@Contacto", nuevaPersona.Contacto);
+
+                        // Ejecutar la consulta
+                        int rowsAffected = command.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Datos guardados correctamente.");
+                            dgvPersonal.Rows.Add(nuevaPersona.Nombre, nuevaPersona.Apellido, nuevaPersona.ID, nuevaPersona.Sector, nuevaPersona.Contacto);
+
+                        }
+                        else
+                        {
+                            MessageBox.Show("No se pudo guardar los datos.");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            //MostrarDatosEnGrilla();
+
+        }
+        private void MostrarDatosEnGrilla()
+        {
+            try
+            {
+                // Cadena de conexión a la base de datos MySQL
+                string connectionString = "Server=localhost;Database=isft225;Uid=root;Pwd=";
+
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = "SELECT Nombre, Apellido, ID, Sector, Contacto FROM isft225";
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
+                        {
+                            DataTable dataTable = new DataTable();
+                            adapter.Fill(dataTable);
+
+                            // Asignar el DataTable al DataGridView
+                            dgvPersonal.DataSource = dataTable;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar los datos: " + ex.Message);
+            }
+        }
+        private void btnEvento_Click(object sender, EventArgs e)
+        {
+            Form1 frmEvento = new Form1();
+            frmEvento.Show();
+        }
+
+        private void dgvPersonal_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private void Eliminar()
+        {
+            try
+            {
+                // Verificar si se hizo clic en una celda y no en la cabecera
+                if (dgvPersonal.SelectedCells.Count > 0)
+                {
+                    // Obtener el ID de la fila seleccionada (suponiendo que el nombre de la columna es "ID")
+                    string idToString = dgvPersonal.SelectedCells[0].OwningRow.Cells["ID"].Value.ToString();
+                    int idToDelete = Convert.ToInt32(idToString);
+                    string connectionString = "Server=localhost;Database=isft225;Uid=root;Pwd=";
+
+                    using (MySqlConnection connection = new MySqlConnection(connectionString))
+                    {
+                        connection.Open();
+
+                        // Consulta SQL para eliminar la fila con el ID seleccionado
+                        string deleteQuery = $"DELETE FROM isft225 WHERE ID = {idToDelete}";
+                        using (MySqlCommand deleteCommand = new MySqlCommand(deleteQuery, connection))
+                        {
+                            int rowsAffected = deleteCommand.ExecuteNonQuery();
+
+                            if (rowsAffected > 0)
+                            {
+                                MessageBox.Show("Fila eliminada correctamente.");
+                                MostrarDatosEnGrilla();
+                                // Actualizar el DataGridView después de eliminar
+                                // Asegúrate de que tengas un adapter y una dataTable configurados correctamente
+                                // adapter.Fill(dataTable);
+                            }
+                            else
+                            {
+                                MessageBox.Show("No se pudo eliminar la fila.");
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Por favor, seleccione una fila para eliminar.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al eliminar la fila: " + ex.Message);
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            Eliminar();
+        }
+
+        private void eventoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1 frmEvento = new Form1();
+            frmEvento.Show();
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Obtén los valores de los cuadros de texto
+                string nombre = txtNombre.Text;
+                string apellido = txtApellido.Text;
+                string id = txtID.Text;
+                string sector = txtSector.Text;
+                string contacto = txtContacto.Text;
+
+                // Verifica que el ID no esté vacío, ya que es necesario para realizar la actualización
+                if (string.IsNullOrWhiteSpace(id))
+                {
+                    MessageBox.Show("El ID es necesario para actualizar los datos.");
+                    return;
+                }
+
+                // Crea una instancia de la clase Persona con los valores
+                Persona personaActualizada = new Persona(nombre, apellido, id, sector, contacto);
+
+                // Cadena de conexión a la base de datos MySQL
+                string connectionString = "Server=localhost;Database=isft225;Uid=root;Pwd=";
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // Consulta de actualización
+                    string query = "UPDATE isft225 SET Nombre = @Nombre, Apellido = @Apellido, Sector = @Sector, Contacto = @Contacto WHERE ID = @ID";
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        // Agregar parámetros
+                        command.Parameters.AddWithValue("@Nombre", personaActualizada.Nombre);
+                        command.Parameters.AddWithValue("@Apellido", personaActualizada.Apellido);
+                        command.Parameters.AddWithValue("@Sector", personaActualizada.Sector);
+                        command.Parameters.AddWithValue("@Contacto", personaActualizada.Contacto);
+                        command.Parameters.AddWithValue("@ID", personaActualizada.ID);
+
+                        // Ejecutar la consulta
+                        int rowsAffected = command.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Datos actualizados correctamente.");
+
+                            // Limpia los cuadros de texto después de actualizar los datos
+                            txtNombre.Clear();
+                            txtApellido.Clear();
+                            txtID.Clear();
+                            txtSector.Clear();
+                            txtContacto.Clear();
+                        }
+                        else
+                        {
+                            MessageBox.Show("No se pudo actualizar los datos. Asegúrate de que el ID sea correcto.");
+                        }
+                    }
+                }
+
+                // Mostrar los datos actualizados en el DataGridView
+                MostrarDatosEnGrilla();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
+}
+
